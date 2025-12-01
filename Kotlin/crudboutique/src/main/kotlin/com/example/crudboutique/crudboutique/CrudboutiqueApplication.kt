f65adc21b122600e70756a21fe4c9c543867c4e7
Gerard <melingandjoug@gmail.com>
2025-12-01 10:31:36 +0100

    New learn

---
+++ Kotlin/crudboutique/src/main/kotlin/com/example/crudboutique/crudboutique/CrudboutiqueApplication.kt
@@ -0,0 +1,11 @@
+package com.example.crudboutique.crudboutique
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrudboutiqueApplication
+
+fun main(args: Array<String>) {
+	runApplication<CrudboutiqueApplication>(*args)
+}
